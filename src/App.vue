4e56b7fab43485fd138aebbd6d3e12640ed8251f
NicolaBellini <bellininicola4366@gmail.com>
2024-05-31 14:20:25 +0200

    [add] scss, bootstrap and getUrl( )

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,23 @@
-<script setup></script>
+<script>
+import axios from "axios";
+export default {
+  methods: {
+    getUrl() {
+      axios
+        .get("http://127.0.0.1:8000/api/projects")
+        .then((res) => {
+          console.log("ciao");
+        })
+        .catch((error) => {
+          console.log("errore");
+        });
+    },
+  },
+  mounted() {
+    this.getUrl();
+  },
+};
+</script>
 
 <template>
   <h1>ciaos</h1>

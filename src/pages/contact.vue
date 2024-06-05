9e310f4e15e3e89694c062bca935eae97bc8e2b1
NicolaBellini <bellininicola4366@gmail.com>
2024-06-05 15:18:27 +0200

    [add] flag logic, for success message

--- src/pages/contact.vue
+++ src/pages/contact.vue
@@ -10,12 +10,14 @@ export default {
       name: "nico",
       mail: "dscsdc@dcsd.com",
       text: "xasxcs dcscsacs cdacas",
-      // store,
+      sending: false,
+      sent: false,
     };
   },
   methods: {
     sendMail() {
-      // console.log("send");
+      this.sending = true;
+
       const data = {
         name: this.name,
         mail: this.mail,
@@ -25,9 +27,12 @@ export default {
       axios
         .post(store.apiUrl + "send-mail", data)
         .then((res) => {
-          console.log(res.data);
+          (this.sending = false),
+            (this.sent = res.data.success),
+            console.log(res.data.success);
         })
         .catch((error) => {
+          (this.sent = true), (this.sending = false);
           console.log(error.message);
         });
     },
@@ -65,21 +70,25 @@ export default {
   <section class="contact">
     <div class="container">
       <h1 class="contact-title">Mettiamoci in contatto</h1>
-      <form @submit.prevent="sendMail" class="contact-form">
-        <div class="form-group">
-          <label for="name">Nome</label>
-          <input v-model="name" type="text" id="name" name="name" required />
-        </div>
-        <div class="form-group">
-          <label for="email">Email</label>
-          <input v-model="mail" type="email" id="mail" name="email" required />
-        </div>
-        <div class="form-group">
-          <label for="message">Messaggio</label>
-          <textarea v-model="text" id="message" name="text" required></textarea>
-        </div>
-        <button type="submit" class="submit-btn">Invia</button>
-      </form>
+      <h2 v-if="sending">Loading</h2>
+      <div v-else>
+        <form v-if="!sent" @submit.prevent="sendMail" class="contact-form">
+          <div class="form-group">
+            <label for="name">Nome</label>
+            <input v-model="name" type="text" id="name" name="name" />
+          </div>
+          <div class="form-group">
+            <label for="email">Email</label>
+            <input v-model="mail" type="email" id="mail" name="email" />
+          </div>
+          <div class="form-group">
+            <label for="message">Messaggio</label>
+            <textarea v-model="text" id="message" name="text"></textarea>
+          </div>
+          <button type="submit" class="submit-btn">Invia</button>
+        </form>
+        <h3 v-else>Email inviata correttamente</h3>
+      </div>
     </div>
   </section>
 </template>

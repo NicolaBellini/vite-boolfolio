03a81201e0be82969731109b2e4265f0324ab87e
NicolaBellini <Bellininicola4366@gmail.com>
2024-07-12 14:24:41 +0200

    [fix] router

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import home from "./pages/home.vue";
-import about from "./pages/about.vue";
+
 import blog from "./pages/blog.vue";
 import contact from "./pages/contact.vue";
 import detailProject from "./pages/detailProject.vue";
@@ -18,11 +18,7 @@ const router = createRouter({
       name: "home",
       component: home,
     },
-    {
-      path: "/about",
-      name: "about",
-      component: about,
-    },
+
     {
       path: "/blog",
       name: "blog",
